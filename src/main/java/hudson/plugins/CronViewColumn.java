6a7945e575dd330126d5821729164895fdb8da7a
Markus Winter <m.winter@sap.com>
2026-01-06 19:19:37 +0100

    support for trigges in plugins
    The plugin extended-timer-trigger and parameterized-scheduler also
    allow to define crons to schedule runs so their cron schedule should
    also be added here if plugins are installed.

--- src/main/java/hudson/plugins/CronViewColumn.java
+++ src/main/java/hudson/plugins/CronViewColumn.java
@@ -12,9 +12,12 @@ import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
 import hudson.views.ListViewColumn;
 import hudson.views.ListViewColumnDescriptor;
+import io.jenkins.plugins.extended_timer_trigger.ExtendedTimerTrigger;
 import java.util.Map;
+import jenkins.model.Jenkins;
 import jenkins.model.ParameterizedJobMixIn;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.parameterizedscheduler.ParameterizedTimerTrigger;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
@@ -31,7 +34,9 @@ import org.kohsuke.stapler.DataBoundSetter;
 public class CronViewColumn extends ListViewColumn {
 
     private static final String CRON_EXPRESSION_COMMENT_START = "#";
+    private static final String EXTENDED_CRON_EXPRESSION_COMMENT_START = "//";
     private static final String CRON_EXPRESSION_COMMENT_COLOR = "var(--dark-green)";
+    private static final String CRON_EXPRESSION_PARAM_COLOR = "var(--blue)";
 
     private boolean hideComments;
 
@@ -45,6 +50,28 @@ public class CronViewColumn extends ListViewColumn {
     @DataBoundSetter
     public void setHideComments(boolean hideComments) {
         this.hideComments = hideComments;
+    }
+
+    private boolean hasPlugin(String pluginName) {
+        return Jenkins.get().getPlugin(pluginName) != null;
+    }
+
+    private String getCronSpec(Trigger<?> trigger) {
+        boolean isExtendedTimerTrigger =
+                hasPlugin("extended-timer-trigger") && trigger instanceof ExtendedTimerTrigger ett;
+
+        boolean isParameterizedTrigger =
+                hasPlugin("parameterized-scheduler") && trigger instanceof ParameterizedTimerTrigger ptt;
+
+        String cronExpression;
+        if (hasPlugin("extended-timer-trigger") && trigger instanceof ExtendedTimerTrigger ett) {
+            return ett.getCronSpec();
+        }
+        if (hasPlugin("parameterized-scheduler") && trigger instanceof ParameterizedTimerTrigger ptt) {
+            return ptt.getParameterizedSpecification();
+        } else {
+            return trigger.getSpec();
+        }
     }
 
     /**
@@ -66,16 +93,20 @@ public class CronViewColumn extends ListViewColumn {
         for (Trigger<?> trigger : triggers.values()) {
             if (trigger == null) continue;
 
-            String cronExpression = trigger.getSpec();
+            String cronExpression = getCronSpec(trigger);
             if (cronExpression == null || cronExpression.isBlank()) continue;
 
-            cronExpression = formatComments(cronExpression, hideComments);
+            cronExpression = formatComments(cronExpression, hideComments, trigger);
 
             // Cron expression can still be set when Source Code Management has been disabled.
             if (!hasSourceCodeManagement && trigger instanceof SCMTrigger) expression.append("<i>(Disabled) </i>");
 
             // Add trigger name and cron expression.
-            expression.append(getTriggerName(trigger)).append(": ").append(cronExpression);
+            expression
+                    .append(getTriggerName(trigger))
+                    .append(":<br/><div class='jenkins-!-margin-left-1'>")
+                    .append(cronExpression)
+                    .append("</div>");
         }
 
         return expression.toString();
@@ -84,8 +115,11 @@ public class CronViewColumn extends ListViewColumn {
     /**
      * Change the font color on the comment text within a cron expression.
      */
-    private String formatComments(String cronExpression, boolean hideComments) {
-        if (!cronExpression.contains(CRON_EXPRESSION_COMMENT_START)) return cronExpression; // No comment found.
+    private String formatComments(String cronExpression, boolean hideComments, Trigger<?> trigger) {
+
+        boolean isExtendedTimerTrigger = hasPlugin("extended-timer-trigger") && trigger instanceof ExtendedTimerTrigger;
+        boolean isParameterizedTrigger =
+                hasPlugin("parameterized-scheduler") && trigger instanceof ParameterizedTimerTrigger;
 
         StringBuilder formattedExpression = new StringBuilder();
 
@@ -95,20 +129,43 @@ public class CronViewColumn extends ListViewColumn {
             if (expressionLine.isBlank()) {
                 continue;
             }
-            int commentStartIndex = expressionLine.indexOf(CRON_EXPRESSION_COMMENT_START);
-            if (commentStartIndex < 0) {
+            boolean hasComment = expressionLine.startsWith(CRON_EXPRESSION_COMMENT_START);
+            boolean hasExtendedComment = isExtendedTimerTrigger
+                    && !expressionLine.startsWith("%")
+                    && expressionLine.contains(EXTENDED_CRON_EXPRESSION_COMMENT_START);
+            int formatIndex = expressionLine.indexOf(EXTENDED_CRON_EXPRESSION_COMMENT_START);
+            if (isExtendedTimerTrigger && expressionLine.startsWith("%")) {
+                expressionLine = "<b style='color: " + CRON_EXPRESSION_PARAM_COLOR + ";'>" + expressionLine + "</b>";
+            }
+            if (!hasComment && !hasExtendedComment) {
                 // No comment, so just add the original expression line.
+                if (isParameterizedTrigger && expressionLine.contains("%")) {
+                    // Highlight parameter references in parameterized cron expressions.
+                    expressionLine = expressionLine.replaceAll(
+                            "(%.*)", "<b style='color: " + CRON_EXPRESSION_PARAM_COLOR + ";'>$1</b>");
+                }
                 formattedExpression.append(expressionLine);
             } else {
                 if (hideComments) {
-                    formattedExpression.append(expressionLine, 0, commentStartIndex);
-                    continue;
+                    if (hasComment || formatIndex == 0) {
+                        // Entire line is a comment, so skip it.
+                        continue;
+                    }
+                    // Extended comment found after expression, append only the expression part.
+                    formattedExpression.append(expressionLine, 0, formatIndex);
                 } else {
-                    // Comment found, wrapping comment in font tags (setting the color).
-                    formattedExpression.append(expressionLine, 0, commentStartIndex);
-                    formattedExpression.append("<b><em style='color: " + CRON_EXPRESSION_COMMENT_COLOR + ";'>");
-                    formattedExpression.append(expressionLine.substring(commentStartIndex));
-                    formattedExpression.append("</em></b>");
+                    if (hasComment) {
+                        // Classic comment found, the complete line is a comment.
+                        formattedExpression.append("<b><em style='color: " + CRON_EXPRESSION_COMMENT_COLOR + ";'>");
+                        formattedExpression.append(expressionLine);
+                        formattedExpression.append("</em></b>");
+
+                    } else {
+                        formattedExpression.append(expressionLine, 0, formatIndex);
+                        formattedExpression.append("<b><em style='color: " + CRON_EXPRESSION_COMMENT_COLOR + ";'>");
+                        formattedExpression.append(expressionLine.substring(formatIndex));
+                        formattedExpression.append("</em></b>");
+                    }
                 }
             }
             formattedExpression.append("<br/>");
